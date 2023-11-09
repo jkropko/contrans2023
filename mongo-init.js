2aae7dd4ec6cd3ec1314a52d0586fc74ce7f9ffe
jkropko <jkropko@gmail.com>
2023-11-09 15:40:29 -0500

    Mongo queries

---
+++ mongo-init.js
@@ -0,0 +1,13 @@
+db.createUser(
+        {
+            user: "jk8sd",
+            pwd: "olivegardenunlimitedbreadsticks",
+            roles: [
+                {
+                    role: "readWrite",
+                    db: "init_db"
+                }
+            ]
+        }
+    )
+;
